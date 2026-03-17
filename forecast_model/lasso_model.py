c9096e51e820854450cbb0bec736d93f38696489
emelchor <melchor.erwis@gmail.com>
2026-03-17 01:14:01 -0600

    15032026

--- forecast_model/lasso_model.py
+++ forecast_model/lasso_model.py
@@ -1,6 +1,7 @@
+from sklearn.linear_model import Lasso
 from forecast_model.base_model import BasePSOForecaster
 class LassoPSOModel(BasePSOForecaster):
 
     def build_model(self):
 
-        return Lasso(alpha=0.01)
+        return Lasso(alpha=0.001)
